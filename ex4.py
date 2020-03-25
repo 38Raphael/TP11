27917e61ab41274110c2cf5e29baa1fd6327ba3c
PCP-RLOUYOT <raphael.louyot@isen-ouest.yncrea.fr>
2020-03-25 09:06:02 +0100

    ex4

---
+++ ex4.py
@@ -0,0 +1,58 @@
+class Duree:
+    def __init__(self, h, m, s):
+        self.__heure = h
+        self.__minute = m
+        self.__seconde = s
+
+    def affduree(self):
+        if self.__heure>= 0 and self.__minute >= 0 and self.__seconde >= 0:
+            if self.__heure < 24:
+                if self.__seconde < 60:
+                    if self.__minute < 60:
+                        return str(self.__heure) + "h" + str(self.__minute) + "m" + str(self.__seconde) + "s"
+                    else:
+                        self.__heure += 1
+                        self.__minute -= 60
+                        return str(self.__heure) + "h" + str(self.__minute) + "m" + str(self.__seconde) + "s"
+                else:
+                    self.__minute += 1
+                    self.__seconde -= 60
+                    if self.__minute < 60:
+                        return str(self.__heure) + "h" + str(self.__minute) + "m" + str(self.__seconde) + "s"
+                    else:
+                        self.__heure += 1
+                        self.__minute -= 60
+                        return str(self.__heure) + "h" + str(self.__minute) + "m" + str(self.__seconde) + "s"
+            else:
+                self.__heure -= 24
+                if self.__seconde < 60:
+                    if self.__minute < 60:
+                        return str(self.__heure) + "h" + str(self.__minute) + "m" + str(self.__seconde) + "s"
+                    else:
+                        self.__heure += 1
+                        self.__minute -= 60
+                        return str(self.__heure) + "h" + str(self.__minute) + "m" + str(self.__seconde) + "s"
+                else:
+                    self.__minute += 1
+                    self.__seconde -= 60
+                    if self.__minute < 60:
+                        return str(self.__heure) + "h" + str(self.__minute) + "m" + str(self.__seconde) + "s"
+                    else:
+                        self.__heure += 1
+                        self.__minute -= 60
+                        return str(self.__heure) + "h" + str(self.__minute) + "m" + str(self.__seconde) + "s"
+        else:
+            return "Choisissez une heure, des minutes et des secondes positives"
+
+    def __add__(self, other):
+        if isinstance(other, Duree):
+            return Duree(self.__heure + other.__heure, self.__minute + other.__minute, self.__seconde + other.__seconde)
+
+
+if __name__ == '__main__':
+    h1 = Duree(25, 36, 74)
+    h2 = Duree(14, 82, 21)
+    print(h1.affduree())
+    print(h2.affduree())
+    h3 = h1 + h2
+    print(h3.affduree())
